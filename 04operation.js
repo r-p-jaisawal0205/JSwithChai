812566371f3b2fbb52671e766104da7367c156f0
Ravi Prakash Jaisawal <rp.jaisawal77@gmail.com>
2024-10-20 03:46:41 +0530

    Third Commit

---
+++ 04operation.js
@@ -0,0 +1,17 @@
+console.log('Welcome to Java Script Data Types Operation & Conversion.');
+console.log("Hello" + "Ravi"); // String concatination
+console.log("1" + "2"); // String concatination
+console.log(1 + "2"); // implicit conversion Number to String
+console.log("1" + 2); // implicit conversion Number to String
+console.log(1 + 2);
+console.log("1" + "2" + 3); // implicit conversion Number to String and operation sequnce left to right
+console.log(1 + "2" + "3"); // implicit conversion Number to String and operation sequnce left to right
+console.log("1" + 2 + "3"); // implicit conversion Number to String and operation sequnce left to right
+console.log(1 + 2 + "3"); // implicit conversion Number to String and operation sequnce left to right
+console.log("1" + 2 + 3); // implicit conversion Number to String and operation sequnce left to right
+console.log("2" - "1"); // implicit conversion String to Number
+console.log("2" - 1); // implicit conversion String to Number
+let num = 0;
+console.log(++num); // pre increment by 1 | current value = 1
+console.log(num++); // post increment by 1 | current value = 2
+console.log(num);
