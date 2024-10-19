201c4ebe90b3f4ea4232be9a651b4f524b4e2727
Ravi Prakash Jaisawal <rp.jaisawal77@gmail.com>
2024-10-20 03:13:06 +0530

    modified:   02dataTypes.js

--- 02dataTypes.js
+++ 02dataTypes.js
@@ -1,1 +1,9 @@
 console.log('Welcome to Java Script Data Types.');
+console.log(typeof "Ravi");
+console.log(typeof 0);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof true);
+console.log(typeof typeof null);
