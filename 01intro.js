fceffd97d5c9bbac8a70393cf1b4285a403f3c8f
Ravi Prakash Jaisawal <rp.jaisawal77@gmail.com>
2024-10-20 02:54:55 +0530

    Initial Commit

---
+++ 01intro.js
@@ -0,0 +1,7 @@
+const accountId = 12345;
+let accountName = "Ravi";
+var accountCity = "Lucknow";
+accountState = "UP";
+let accountCountry;
+
+console.table([accountId, accountName, accountCity, accountState, accountCountry]);
